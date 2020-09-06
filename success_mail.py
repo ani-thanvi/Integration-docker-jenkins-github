c793c0e4e1db706343fbdcbaf48b09d02c1c2682
Anirudhi Thanvi <42400848+ani-thanvi@users.noreply.github.com>
2020-09-07 00:52:54 +0530

    success_mail.py

---
+++ success_mail.py
@@ -0,0 +1,20 @@
+  import smtplib 
+
+	# creates SMTP session 
+	s = smtplib.SMTP('smtp.gmail.com', 587) 
+
+	# start TLS for security 
+	s.starttls()   
+
+	# Authentication 
+	s.login("sender_email", "password")
+ 
+	# message to be sent 
+	message = "Hey Developer, Finally we got the model trained. "
+
+	# sending the mail 
+
+	s.sendmail("sender_mail", "developr_mail", message)
+	# terminating the session 
+
+	s.quit()
