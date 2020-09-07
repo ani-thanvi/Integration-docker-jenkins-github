247e4ba31dc0f93ce672b9ddc42d8a09389d6e39
Anirudhi Thanvi <42400848+ani-thanvi@users.noreply.github.com>
2020-09-07 09:16:57 +0000

    updated

--- success_mail.py
+++ success_mail.py
@@ -1,20 +1,20 @@
-        import smtplib 
-
-	# creates SMTP session 
-	s = smtplib.SMTP('smtp.gmail.com', 587) 
-
-	# start TLS for security 
-	s.starttls()   
-
-	# Authentication 
-	s.login("sender_email", "password")
- 
-	# message to be sent 
-	message = "Hey Developer, Finally we got the model trained. "
-
-	# sending the mail 
-
-	s.sendmail("sender_mail", "developr_mail", message)
-	# terminating the session 
-
-	s.quit()
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+host_address="***@gmail.com"
+host_pass="****"
+guest_address="***@gmail.com"
+subject="Success"
+content="Success"
+message = MIMEMultipart()
+message['From']=host_address
+message['To']=guest_address
+message['Subject']=subject
+message.attach(MIMEText(content,'plain'))
+session= smtplib.SMTP('smtp.gmail.com',587)
+session.starttls()
+session.login(host_address,host_pass)
+text=message.as_string()
+session.sendmail(host_address,guest_address,text)
+session.quit()
+print("Mail sent successfully")
